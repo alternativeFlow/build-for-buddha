48f74dfea2b56663dae4dd9db725c173d80e5606
alternativeFlow <danxue@gmail.com>
2018-08-05 11:53:47 -0700

    replaced create-react-app boilerplate

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Route } from 'react-router-dom';
+import HomePage from './components/HomePage';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+
+		};
+	}
+
+	render() {
+
+		return(
+		  <div>
+		    <Route location={this.props.location} path="/" exact component={HomePage} />
+ 		  </div>
+		);
+	}
 }
+
+App.propTypes = {
+  location: PropTypes.shape({
+    pathname: PropTypes.string.isRequired
+  }).isRequired
+};
 
 export default App;
